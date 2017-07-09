2cc6fc4e81296bb1a0523be3c56227c57660d784
Brendan Bard <bbard@purdue.edu>
2017-07-09 18:00:10 -0400

    added most of content

---
+++ app.js
@@ -0,0 +1,5 @@
+showBio =(ev) =>{
+    const bio = document.getElementById('bioContainer')
+    bio.style.display = "block"
+    bio.textContent = "Hello my name is Brendan, I am a cybersecurity student at Purdue University. I am passionate about technical theater, video games, and ethical hacking. I have been apart of the runCMD() team at Purdue since its conception in 2016. We have participated in the Collegiate Cyber Defense Competition and National Cyber League as we strive to get a deeper understanding of cybersecurity. Contact me by email at bbard@purdue.edu. "
+}
